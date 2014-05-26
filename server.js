b5cc447c3451d11d955f044a847c0c8d19200ce3
Peter Beňo <najlepsiwebdesigner@gmail.com>
2014-05-26 15:06:30 +0200

    add frame skipping and broadcasting speed logging

--- server.js
+++ server.js
@@ -31,18 +31,59 @@ function handler (req, res) {
   }
 }
 
+
+var dataBroadcasted = 0; // in bytes
+var startTime = process.hrtime()[0]; // in seconds
+var time, previousTime = 0;
+
+
+
 var kcontext = kinect();
 
 kcontext.resume();
 
 kcontext.start('depth');
 
+
+// simple frame skipping counter
+var counter = 0;
+
+
+
+
 var kstream = new BufferStream();
 
 kcontext.on('depth', function (buf) {
 
-  kstream.write(buf);
+  if (counter % 3 == 0) { // keep every 25th frame
+      kstream.write(buf);
+      dataBroadcasted += toArrayBuffer(buf).byteLength;
+
+  }
+  counter++;
+
+  if (counter == 100) {
+    counter = 0;
+  }
+  
+  time = process.hrtime()[0] - startTime;
+
+  if (time != previousTime) {
+    console.log("Broadcasting data ~" + (dataBroadcasted / 1048576).toFixed(2) + " MB/s"); // in megabytes
+    previousTime = time;
+    dataBroadcasted = 0;
+  }
+
+  // console.log(process.hrtime());
+
+  // exit nodejs code
+  // process.exit(code=0)
+  // process.exit(1);
 });
+
+
+
+
 
 
 wss.on('connection', function(ws) {
@@ -57,4 +98,28 @@ wss.on('connection', function(ws) {
 });
 
 
+
+
+
+// helper functions for buffers from 
+// http://stackoverflow.com/questions/8609289/convert-a-binary-nodejs-buffer-to-javascript-arraybuffer
+
+function toBuffer(ab) {
+    var buffer = new Buffer(ab.byteLength);
+    var view = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) {
+        buffer[i] = view[i];
+    }
+    return buffer;
+}
+
+
+function toArrayBuffer(buffer) {
+    var ab = new ArrayBuffer(buffer.length);
+    var view = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) {
+        view[i] = buffer[i];
+    }
+    return ab;
+}
 
